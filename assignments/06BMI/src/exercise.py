16c43f87efcd5788ce75c5eee4292063dc1d184b
AngelNoe <89592815+AngelNoe@users.noreply.github.com>
2021-09-08 16:44:22 -0500

    Manualmente
    No pude hacer el commit desde visual studio code

--- assignments/06BMI/src/exercise.py
+++ assignments/06BMI/src/exercise.py
@@ -2,7 +2,19 @@ def main():
     # escribe tu código abajo de esta línea
     peso = float(input("Peso en kg: "))
     altura = float(input("Altura en m: "))
-
+    indice = peso/(altura**2)
+    if indice == 0:
+        print("Revisa tus datos, alguno de ellos es erróneo")
+    elif indice < 20:
+        print("PESO BAJO")
+    elif indice <= 20 and indice < 25:
+        print("NORMAL")
+    elif indice <= 25 and indice < 30:
+        print("SOPREPESO")
+    elif indice <= 30 and indice < 40:
+        print("OBESIDAD")
+    else:
+        print("OBESIDAD MORBIDA")
 
 if __name__=='__main__':
     main()
